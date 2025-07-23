3b8390d2c5d1cd18aa15b9ce00b80e7c2943b710
PoojaK <poojakarthiks28@gmail.com>
2025-07-23 15:38:59 -0500

    Initial Commit

---
+++ InteractiveUi/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  InteractiveUi
+//
+//  Created by Pooja Karthikeyan on 7/23/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ InteractiveUi/InteractiveUiApp.swift
@@ -0,0 +1,17 @@
+//
+//  InteractiveUiApp.swift
+//  InteractiveUi
+//
+//  Created by Pooja Karthikeyan on 7/23/25.
+//
+
+import SwiftUI
+
+@main
+struct InteractiveUiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
